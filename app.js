e2b80a9087c32a5e9161e6578266dff72c8ce155
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-08-12 17:55:36 +0600

    RealTime Tracker project is done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { SlowBuffer } = require("buffer");
 const express = require("express");
 const app = express();
 
@@ -9,10 +10,22 @@ const server = http.createServer(app);
 const io =  socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket){
+
+    socket.on("sent-location", function(locationData){
+        io.emit("receive-location", {id: socket.id, ...locationData })
+    });
+    console.log("connected");
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id);
+    })
+})
 
 app.get("/", function(req, res){
-    res.send("hello");
-
+    res.render("index");
 })
-server.listen(3000);
+server.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+});
